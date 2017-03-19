b5d3b8866f4cc21cd55a5f30d07a6143b19b4cf8
Xueyi Zou <xy.zou@outlook.com>
2017-03-19 17:59:32 +0800

    change project name

--- ACASX _3D_Testing/MatlabCode/maxAccidentRate.m
+++
@@ -1,9 +0,0 @@
-function value = maxAccidentRate(x)
-    global testPoints;
-    global AR;
-    global count;
-    accidentRate = AR.value(x);
-    value =1.0-accidentRate;  
-    testPoints=[testPoints; x', accidentRate];
-    count = count +1;
-
